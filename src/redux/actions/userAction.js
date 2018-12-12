1751c466446588be3d33b012537158980d85e7d8
Troy Amelotte <troyamelotte@gmail.com>
2018-12-11 21:39:30 -0700

    small fix

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,4 +1,4 @@
-import {REGISTER_USER_SUCCESS, LOGIN_USER_SUCCESS, ERROR_MESSAGE, SUCCESS_MESSAGE, VERIFY_USER_SUCCESS} from "../types";
+import {REGISTER_USER_SUCCESS, LOGIN_USER_SUCCESS} from "../types";
 import axios from "../../axios";
 
 export const userLogin = (user) => dispatch => {
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
- import {LOGIN_USER_SUCCESS, REGISTER_USER_SUCCESS, VERIFY_USER_SUCCESS} from "../types";
+ import {LOGIN_USER_SUCCESS, REGISTER_USER_SUCCESS} from "../types";
 
 const initialState = {user:{}, token: localStorage.getItem('token')};
 export default (state = initialState, action) => {
@@ -6,10 +6,7 @@ export default (state = initialState, action) => {
         case REGISTER_USER_SUCCESS:
             return state
         case LOGIN_USER_SUCCESS:
-            localStorage.setItem('token', action.payload.token);
-            return {user: action.payload.user, token: action.payload.token}
-        case VERIFY_USER_SUCCESS:
-            return {...state, user: action.payload}
+            return state
         default:
             return state
     }
